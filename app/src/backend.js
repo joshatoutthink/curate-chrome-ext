90870de40181222cdcb8c9bda12bde87411ce2b9
Joshua Kennedy <josh@outthinkgroup.com>
2020-11-18 20:18:58 -0500

    ContextMenu action to show up, and saving img url

--- app/src/backend.js
+++ app/src/backend.js
@@ -1,1 +1,33 @@
+//add context menu item
+chrome.contextMenus.create({
+  id: "curate-this-item",
+  title: "Add To Collection",
+  onclick: handleContextMenuClick,
+  contexts: ["image", "video"],
+});
+//context menu function
+function handleContextMenuClick({ srcUrl, pageUrl, mediaType }) {
+  //create entry
+  const entry = {
+    srcUrl,
+    pageUrl,
+    mediaType,
+  };
+  // send url to storage
+  let collection = [];
+  chrome.storage.sync.get(["collection"], function getPreviousCollection(
+    result
+  ) {
+    if (Object.is(result, {})) {
+      return;
+    }
+    collection = result.collection;
+  });
 
+  chrome.storage.sync.set({ collection: [...collection, entry] });
+  chrome.storage.sync.get(["collection"], function getPreviousCollection(
+    result
+  ) {
+    console.log(result);
+  });
+}
